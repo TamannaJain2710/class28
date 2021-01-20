96a0d26a7d8ef573c678284c24c0d89c53c57f9d
Tamanna <thecreativefeather725@gmail.com>
2021-01-20 16:56:19 +0530

    .

--- sketch.js
+++ sketch.js
@@ -7,6 +7,9 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 var bird, slingShot;
+var play = 1;
+var end = 0;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,10 +39,10 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingShot = new SlingShot(bird.body,{x:200,y:100});
+    slingShot = new SlingShot(bird.body,{x:200,y:50});
 }
 
 function draw(){
